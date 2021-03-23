67bcdfa0edf8f21768019d8a9683bf3a228d728d
valentinagonzalezF <v.gonzalez.2@ug.uchile.cl>
2021-03-22 22:55:41 -0300

    Actions and reducers

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,17 +1,101 @@
+import axios from "axios";
+
+import {
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    USER_LOADING
+} from "./types";
 
 // CHECK TOKEN & LOAD USER
-export const loadUser = () => async (dispatch, getState) => {
+export const loadUser = () => (dispatch, getState) => {
+    // User Loading
+    dispatch({ type: USER_LOADING });
+    axios
+        .get(process.env.REACT_APP_API_URL + '/auth/user', tokenConfig(getState))
+        .then((res) => {
+            dispatch({
+                type: USER_LOADED,
+                payload: res.data,
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: AUTH_ERROR,
+            });
+        });
 };
 
 // LOGIN USER
 export const login = (email, password) => async (dispatch) => {
+    // Headers
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    // Request Body
+    const body = {
+        email: email,
+        password: password,
+    };
+
+    await axios
+        .post(process.env.REACT_APP_API_URL + "/login/", body, config)
+        .then((res) => {
+            if (res.data.statusCode === 200) {
+                dispatch({
+                    type: LOGIN_SUCCESS,
+                    payload: res.data,
+                });
+            } else {
+                dispatch({
+                    type: LOGIN_FAIL,
+                    payload: "Usuario y/o contraseña incorrectos",
+                });
+            }
+        })
+        .catch((err) => {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: "Usuario y/o contraseña incorrectos",
+            });
+        });
 };
 
 // LOGOUT USER
 export const logout = () => async (dispatch, getState) => {
+    await axios
+        .post(`${process.env.REACT_APP_API_URL}/logout/`)
+        .then((res) => {
+            dispatch({
+                type: LOGOUT_SUCCESS,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 // Setup config with token
 export const tokenConfig = (getState) => {
+    // Get token from state
+    const access_token = getState().auth.access_token;
 
+    // Headers
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    // If token, add to headers config
+    if (access_token) {
+        config.headers["Authorization"] = `Token ${access_token}`;
+    }
+
+    return config;
 };
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,8 +1,73 @@
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+} from "../actions/types";
+
 const initialState = {
+    access_token: localStorage.getItem("access_token"),
+    isAuthenticated: null,
+    isLoading: true,
+    usuario: null,
+    error: null,
 };
 
 function auth(state = initialState, action) {
     switch (action.type) {
+        case USER_LOADING:
+            return { ...state, isLoading: true, error: null };
+        case USER_LOADED:
+            localStorage.setItem("access_token", action.payload.access);
+            return {
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                error: null,
+                access_token: action.payload.access,
+                usuario: action.payload.usuario,
+            };
+        case LOGIN_SUCCESS:
+            localStorage.setItem("access_token", action.payload.access);
+            return {
+                ...state,
+                access_token: action.payload.access,
+                usuario: action.payload.usuario,
+                error: null,
+                isAuthenticated: true,
+                isLoading: false,
+            };
+        case LOGIN_FAIL:
+            localStorage.removeItem("access_token");
+            return {
+                ...state,
+                error: action.payload,
+                access_token: null,
+                usuario: null,
+                isAuthenticated: false,
+                isLoading: false,
+            };
+        case AUTH_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+                access_token: null,
+                usuario: null,
+                isAuthenticated: false,
+                isLoading: false,
+            };
+        case LOGOUT_SUCCESS:
+            localStorage.removeItem("access_token");
+            return {
+                ...state,
+                error: null,
+                access_token: null,
+                usuario: null,
+                isAuthenticated: false,
+                isLoading: false,
+            };
         default:
             return state;
     }
