15ad16a3696ddbc7a01d212a9dd78813480f7c40
David Escobar <david.escobar@unholster.com>
2022-05-04 18:54:58 -0400

    Code style

--- src/views/Charts/queries.js
+++ src/views/Charts/queries.js
@@ -2,19 +2,23 @@ import {getToken} from "../../utils/axios";
 import axios from "axios";
 
 const STATIONS_URL = `${process.env.REACT_APP_API_URL}/api/public/stations`
+const POLLUTANTS_URL = `${process.env.REACT_APP_API_URL}/api/pollutants`
+
+const token = getToken();
 
 export async function getStationsChoices() {
 
-    const token = getToken();
-
-    const r = await axios.get(STATIONS_URL, {
+    const queryConfig = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
-    })
+    }
+
+    const r = await axios.get(STATIONS_URL, queryConfig)
 
     const {data} = r
     const {stations} = data
+
     return stations.map((station) => {
         return {
             'value': station,
@@ -23,21 +27,20 @@ export async function getStationsChoices() {
     })
 }
 
-const POLLUTANTS_URL = `${process.env.REACT_APP_API_URL}/api/pollutants`
 
 export async function getPollutantsChoices() {
 
-    const token = getToken();
-
-    const configQuery = {
+    const queryConfig = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
 
-    const r = await axios.get(POLLUTANTS_URL, configQuery)
-    const {data} = r
-    const {pollutants} = data
+    const r = await axios.get(POLLUTANTS_URL, queryConfig)
+
+    const { data } = r
+    const { pollutants } = data
+
     pollutants.sort()
 
     return pollutants.map((pollutant) => {
