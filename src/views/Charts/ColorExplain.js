4791df7ca53b859d86176b9ebd4a1441a0a412c9
David Escobar <david.escobar@unholster.com>
2022-06-15 17:27:17 -0400

    Data

--- src/views/Charts/ColorExplain.js
+++ src/views/Charts/ColorExplain.js
@@ -23,25 +23,27 @@ export const ColorExplainByPollutant = ({pollutantName}) => {
 
     const isLoaded = Object.keys(thresholdData).length > 0
 
+    const { unit } = !isLoaded ? {} : Pollutant
+
     return !isLoaded ? null : <InfoContainer>
         <ul>
             <ListItem color={colors.LessThanGood}>
-                Menor a {good} ({Pollutant.unit})
+                Menor a {good} ({unit})
             </ListItem>
             <ListItem color={colors.BetweenGoodAndModerate}>
-                Entre {good} ({Pollutant.unit}) y {moderate} ({Pollutant.unit})
+                Entre {good} ({unit}) y {moderate} ({unit})
             </ListItem>
             <ListItem color={colors.BetweenModerateAndUnhealthy}>
-                Entre {moderate} ({Pollutant.unit}) y {unhealthy} ({Pollutant.unit})
+                Entre {moderate} ({unit}) y {unhealthy} ({unit})
             </ListItem>
             <ListItem color={colors.BetweenUnhealthyAndVeryUnhealthy}>
-                Entre {unhealthy} ({Pollutant.unit}) y {very_unhealthy} ({Pollutant.unit})
+                Entre {unhealthy} ({unit}) y {very_unhealthy} ({unit})
             </ListItem>
             <ListItem color={colors.BetweenVeryUnhealthyAndDangerous}>
-                Entre {very_unhealthy} ({Pollutant.unit}) y {dangerous} ({Pollutant.unit})
+                Entre {very_unhealthy} ({unit}) y {dangerous} ({unit})
             </ListItem>
             <ListItem color={colors.MoreThanDangerous}>
-                Mayor a {dangerous} ({Pollutant.unit})
+                Mayor a {dangerous} ({unit})
             </ListItem>
         </ul>
 
